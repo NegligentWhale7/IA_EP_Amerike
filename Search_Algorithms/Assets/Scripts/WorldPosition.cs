6469063fb33ca6682711603196c0878706cad508
Alam Armas <carlosalam33@hotmail.com>
2023-04-24 13:30:06 -0600

    Posición arreglada

--- Search_Algorithms/Assets/Scripts/WorldPosition.cs
+++ Search_Algorithms/Assets/Scripts/WorldPosition.cs
@@ -5,33 +5,21 @@ using UnityEngine.Tilemaps;
 
 public class WorldPosition : MonoBehaviour
 {
-    [Header("Cells")]
-    public Grid AGrid;
-    [SerializeField] private float cellSize = 1f;
-    [SerializeField] ScanArea scan;
-
-    Vector3 mousePos, worldPosition;
-    Vector3Int gridPos;
+    public Tilemap tilemap;
+    public Grid grid;
+    public float cellSize;
+    public ScanArea Scan;
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if(Input.GetMouseButtonDown(0))
         {
-            GetMousePosition();
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int cellPosition = grid.WorldToCell(new Vector3(mousePosition.x, mousePosition.y, 0f));
+            Vector3 worldPosition = grid.CellToWorld(cellPosition) + new Vector3(cellSize / 2f, cellSize / 2f, 0f);
+            Debug.Log("Clicked on cell " + cellPosition + " at position " + worldPosition);
+            Scan.Origin = cellPosition;
+            Scan.StartCoroutine(Scan.FloodField(0.01f));
         }
-    }
-
-
-    private void GetMousePosition()
-    {
-        mousePos = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0f));
-        gridPos = AGrid.WorldToCell(mousePos);
-        Vector3Int gridPosInt= new Vector3Int (gridPos.x, gridPos.y, 0); 
-        worldPosition = AGrid.CellToWorld(gridPos) + new Vector3(cellSize / 2f, cellSize / 2f, 0f);
-        Vector3Int worldPosInt = new Vector3Int ((int)worldPosition.x, (int)worldPosition.y, 0); 
-
-        Debug.Log("Posición del mundo: " + worldPosition);
-        scan.Origin = worldPosInt;
-        scan.StartScan();
     }
 }
